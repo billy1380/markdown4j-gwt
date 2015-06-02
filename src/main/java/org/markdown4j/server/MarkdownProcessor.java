dd0571a8fbf37f60640314c656db56163f3d8926
billy1380 <william.shakour@gmail.com>
2015-06-02 09:07:33 +0100

    - added check for non character after : - updated pom to add junit - updated pom to use emoji-gwt 1.1 - added first test class, to test emoji bug

--- src/main/java/org/markdown4j/server/MarkdownProcessor.java
+++ src/main/java/org/markdown4j/server/MarkdownProcessor.java
@@ -9,20 +9,47 @@ package org.markdown4j.server;
 
 import org.markdown4j.AbstractMarkdownProcessor;
 
+import com.github.rjeschke.txtmark.Decorator;
+import com.github.rjeschke.txtmark.EmojiEmitter;
+import com.github.rjeschke.txtmark.MarkdownUtils;
+
 /**
  * @author William Shakour (billy1380)
  *
  */
 public class MarkdownProcessor extends AbstractMarkdownProcessor {
 
-	/*
-	 * (non-Javadoc)
+	/* (non-Javadoc)
 	 * 
-	 * @see org.markdown4j.AbstractMarkdownProcessor#registerPlugins()
-	 */
+	 * @see org.markdown4j.AbstractMarkdownProcessor#registerPlugins() */
 	@Override
-	protected void registerPlugins() {
-		registerPlugins(new IncludePlugin(), new WebSequencePlugin());
+	protected void registerPlugins () {}
+
+	@Override
+	protected EmojiEmitter emojiEmitter () {
+		return new EmojiEmitter() {
+
+			@Override
+			public void emitEmoji (StringBuilder out, String name,
+					Decorator decorator) {
+				String link = "http://www.emoji.org/" + name;
+				String comment = name + " emoji";
+
+				decorator.openImage(out);
+				out.append(" class=\"emoji\" src=\"");
+				MarkdownUtils.appendValue(out, link, 0, link.length());
+				out.append("\" alt=\"");
+				MarkdownUtils.appendValue(out, name, 0, name.length());
+				out.append('"');
+				if (comment != null) {
+					out.append(" title=\"");
+					MarkdownUtils
+							.appendValue(out, comment, 0, comment.length());
+					out.append('"');
+				}
+				out.append(" />");
+			}
+		};
 	}
 
 }
---
+++ src/test/java/org/markdown4j/MarkdownProcessorTest.java
@@ -0,0 +1,24 @@
+//
+//  MarkdownProcessorTest.java
+//  markdown4j-gwt
+//
+//  Created by William Shakour (billy1380) on 2 Jun 2015.
+//  Copyright © 2015 WillShex Limited. All rights reserved.
+//
+package org.markdown4j;
+
+/**
+ * @author billy1380
+ *
+ */
+public class MarkdownProcessorTest {
+
+	AbstractMarkdownProcessor test = new AbstractMarkdownProcessor() {
+
+		@Override
+		protected void registerPlugins () {
+			
+		}
+	};
+
+}
